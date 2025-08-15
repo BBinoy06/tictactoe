eff5e562837b5a1c410e6931c405ec38f6e1efde
N1304095 <beneetta.binoy2024@my.ntu.ac.uk>
2025-08-15 21:03:27 +0100

    update

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-# class Board:
-#     def __init__(self):
-#     def reset_board(self):
-#     def place_mark(self, row, col, mark):
-#     def check_win(self):
-#     def is_full(self):
+class Board:
+    def __init__(self):
+    def reset_board(self):
+    def place_mark(self, row, col, mark):
+    def check_win(self):
+    def is_full(self):
 
-# class Player:
-#     def __init__(self, name, mark):
-#     def get_move(self, board):
+class Player:
+    def __init__(self, name, mark):
+    def get_move(self, board):
 
-# class Computer:
-#     def __init__(self, mark):
-#     def get_move(self, board):
+class Computer:
+    def __init__(self, mark):
+    def get_move(self, board):
 
-# class Game:
-#     def __init__(self):
-#     def switch_turn(self):
-#     def is_over(self):
-#     def play_turn(self):
+class Game:
+    def __init__(self):
+    def switch_turn(self):
+    def is_over(self):
+    def play_turn(self):
