ccfd918fefae3e0d3dc73ba972d4c6da8d9e5a2d
N1304095 <beneetta.binoy2024@my.ntu.ac.uk>
2025-08-23 15:13:31 +0100

    update

--- testcode_app.py
+++ testcode_app.py
@@ -3,6 +3,7 @@ from app import Board, Player, Computer
 
 class TestBoard(unittest.TestCase):
     def test_board_initialisation(self):
+        board = Board()
         self.assertEqual(board.grid, [[" "] * 3 for _ in range(3)]
 
 if __name__ == "__main__":
