205796465c67229403581b96c8758836ea66588b
N1304095 <beneetta.binoy2024@my.ntu.ac.uk>
2025-08-23 15:12:32 +0100

    update first test:Board

--- testcode_app.py
+++ testcode_app.py
@@ -1,2 +1,9 @@
 import unittest
 from app import Board, Player, Computer
+
+class TestBoard(unittest.TestCase):
+    def test_board_initialisation(self):
+        self.assertEqual(board.grid, [[" "] * 3 for _ in range(3)]
+
+if __name__ == "__main__":
+    unittest.main()
