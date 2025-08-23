97c9588b3b7b59ce4915e5b4076553acc91ea21b
N1304095 <beneetta.binoy2024@my.ntu.ac.uk>
2025-08-23 15:16:20 +0100

    update first test:Player

--- testcode_app.py
+++ testcode_app.py
@@ -6,5 +6,11 @@ class TestBoard(unittest.TestCase):
         board = Board()
         self.assertEqual(board.grid, [[" "] * 3 for _ in range(3)]
 
+class TestPlayer(unittest.TestCase):
+    def test_player_initialistion(self):
+        player = Player("BB", "X")
+        self.assertEqual(player.name, "BB")
+        self.assertEqual(player.mark, "X")
+
 if __name__ == "__main__":
     unittest.main()
